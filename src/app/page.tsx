567555c48018c51d69cfd77b979a20cc58c2ce11
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2024-11-09 14:30:14 -0300

    refactor: accordion component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Checkbox from "@/components/Checkbox/Checkbox";
+import Accordion from "@/components/Accordion/Accordion";
 
 export default function Home() {
     return (
         <div className="bg-theme_neutral text-theme_neutral-0 min-h-screen w-screen flex items-center justify-center p-8">
-            <Checkbox label="Checkbox 1" />
-            <Checkbox label="Checkbox 2" checked />
-            <Checkbox label="Checkbox 3" disabled />
+            <Accordion />
         </div>
     );
 }
