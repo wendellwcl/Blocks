58a6b9749b7d41ed07cd766ef5b5a3fb162c1386
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2024-12-06 22:05:00 -0300

    navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
-import Tooltip from "@/components/Tooltip/Tooltip";
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
     return (
-        <div className="bg-theme_neutral text-theme_neutral-0 min-h-screen w-screen flex items-center justify-center p-8">
-            <div className="absolute top-1/2 left-1/2">
-                <Tooltip tip_text="Hello world">
-                    <div className="underline">Hover me</div>
-                </Tooltip>
-            </div>
-        </div>
+        <>
+            {/* <div className="bg-theme_neutral text-theme_neutral-0 min-h-screen w-screen flex items-center justify-center p-8"> */}
+            {/* </div> */}
+            <Navbar
+                brand={{ brandImgPath: "#", brandAltText: "<Brand/>", brandUrl: "/" }}
+                navLinks={[
+                    { text: "link 1", url: "/" },
+                    { text: "link 2", url: "/" },
+                    { text: "link 3", url: "/" },
+                ]}
+            />
+            <div className="pb-[1000px]"></div>
+        </>
     );
 }
