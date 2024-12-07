769b8e8ad2e025725cf14fe84519d55652b15798
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2024-12-07 16:38:12 -0300

    contextMenu component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,24 @@
-import Navbar from "@/components/Navbar/Navbar";
+import ContextMenu from "@/components/ContextMenu/ContextMenu";
 
 export default function Home() {
     return (
-        <>
-            {/* <div className="bg-theme_neutral text-theme_neutral-0 min-h-screen w-screen flex items-center justify-center p-8"> */}
-            {/* </div> */}
-            <Navbar
-                brand={{ brandImgPath: "#", brandAltText: "<Brand/>", brandUrl: "/" }}
-                navLinks={[
-                    { text: "link 1", url: "/" },
-                    { text: "link 2", url: "/" },
-                    { text: "link 3", url: "/" },
-                ]}
-            />
-            <div className="pb-[1000px]"></div>
-        </>
+        <div className="bg-theme_neutral text-theme_neutral-0 min-h-screen w-screen flex items-center justify-center p-8">
+            <div className="w-16 h-16">
+                <div className="bg-theme_brand rounded p-1 w-full h-full">Context Menu</div>
+                <ContextMenu>
+                    <ul>
+                        <li className="cursor-pointer">Item 1</li>
+                        <li className="cursor-pointer">Item 2</li>
+                        <li className="cursor-pointer">Item 3</li>
+                        <li className="cursor-pointer">Item 4</li>
+                        <li className="cursor-pointer">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque corrupti quam itaque
+                            deserunt? Sint tenetur numquam, cupiditate consequatur velit iure quidem. Perspiciatis natus
+                            voluptas ipsum expedita tempore fugiat, quisquam velit.
+                        </li>
+                    </ul>
+                </ContextMenu>
+            </div>
+        </div>
     );
 }
---
+++ src/components/ContextMenu/ContextMenu.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { MouseEvent as ReactMouseEvent, useEffect, useRef, useState } from "react";
+
+import styles from "./ContextMenu.module.scss";
+
+interface ContextMenuProps {
+    children: React.ReactNode;
+}
+
+export default function ContextMenu({ children }: ContextMenuProps) {
+    const triggerAreaRef = useRef<HTMLDivElement>(null);
+    const floatingMenuRef = useRef<HTMLDivElement>(null);
+    const [position, setPosition] = useState<{ xAxis: number; yAxis: number }>({ xAxis: 0, yAxis: 0 });
+
+    //This function displays a custom context menu when a right-click (or similar event) occurs.
+    // It calculates the click position relative to the trigger area and adjusts the menu's position
+    // to ensure it fits within the viewport. The menu is made visible and styled dynamically.
+    function showContextMenu(e: ReactMouseEvent): void {
+        e.preventDefault(); // Prevent the default context menu from appearing
+
+        // Calculate the click coordinates relative to the trigger area
+        const boundingRect = triggerAreaRef.current!.getBoundingClientRect();
+        const xAxis = e.clientX - boundingRect.left;
+        const yAxis = e.clientY - boundingRect.top;
+        setPosition({ xAxis, yAxis }); // Update the position state
+
+        // Get dimensions of the floating menu
+        const floatingMenuEl = floatingMenuRef.current!;
+        const floatingMenuWidth = floatingMenuEl.clientWidth;
+        const floatingMenuHeight = floatingMenuEl.clientHeight;
+
+        // Check if the floating menu overflows the viewport horizontally
+        if (e.clientX + floatingMenuWidth >= window.innerWidth - 40) {
+            floatingMenuEl.classList.add("right"); // Add a class to adjust its position to the left
+        } else {
+            floatingMenuEl.classList.remove("right"); // Remove the class if no adjustment is needed
+        }
+
+        // Check if the floating menu overflows the viewport vertically
+        if (e.clientY + floatingMenuHeight >= window.innerHeight - 40) {
+            floatingMenuEl.classList.add("bottom"); // Add a class to adjust its position upward
+        } else {
+            floatingMenuEl.classList.remove("bottom"); // Remove the class if no adjustment is needed
+        }
+
+        // Display the floating menu
+        floatingMenuEl.classList.add("show");
+
+        return;
+    }
+
+    // This function hides the custom context menu.
+    function closeContextMenu(): void {
+        const floatingMenuEl = floatingMenuRef.current!;
+
+        // Remove classes to hide the floating menu and reset its position
+        floatingMenuEl.classList.remove("show");
+        floatingMenuEl.classList.remove("right");
+        floatingMenuEl.classList.remove("bottom");
+
+        return;
+    }
+
+    useEffect(() => {
+        // Set the parent element's position to absolute to ensure correct positioning
+        const parentEl = triggerAreaRef.current!.parentElement;
+        parentEl!.style.position = "absolute";
+
+        // Add a global click listener to close the context menu
+        window.addEventListener("click", closeContextMenu);
+
+        // Cleanup the event listener on component unmount
+        return () => {
+            window.removeEventListener("click", closeContextMenu);
+        };
+    }, []);
+
+    return (
+        <>
+            <div
+                className={styles.contextMenu__triggerArea}
+                ref={triggerAreaRef}
+                onContextMenu={(e) => showContextMenu(e)}
+            ></div>
+            <div
+                className={styles.contextMenu__floatingMenu}
+                ref={floatingMenuRef}
+                style={{ top: `${position.yAxis}px`, left: `${position.xAxis}px` }}
+            >
+                {children}
+            </div>
+        </>
+    );
+}
