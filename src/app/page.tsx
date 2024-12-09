789df03baf7ef176bb8323f3cf417ee2b00f8f68
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2024-12-08 23:46:10 -0300

    tabs component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,89 @@
-import SpeedDial from "@/components/SpeedDial/SpeedDial";
+import Tabs from "@/components/Tabs/Tabs";
 
 export default function Home() {
     return (
-        <div className="bg-theme_neutral text-theme_neutral-0 min-h-screen w-screen flex items-center justify-center p-8">
-            <SpeedDial />
+        <div className="bg-theme_neutral text-theme_neutral-0 min-h-screen w-screen h-screen flex items-center justify-center p-8">
+            <Tabs>
+                <div id="id1" data-header-text="content 1">
+                    1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi optio, perspiciatis error minus
+                    rerum enim quos quas voluptatem voluptatum soluta hic delectus labore odio incidunt asperiores cum
+                    quasi veritatis ab.
+                </div>
+                <div id="id2" data-header-text="content 2">
+                    2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi optio, perspiciatis error minus
+                    rerum enim quos quas voluptatem voluptatum soluta hic delectus labore odio incidunt asperiores cum
+                    quasi veritatis ab.
+                </div>
+                <div id="id3" data-header-text="content 3">
+                    3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi optio, perspiciatis error minus
+                    rerum enim quos quas voluptatem voluptatum soluta hic delectus labore odio incidunt asperiores cum
+                    quasi veritatis ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi optio,
+                    perspiciatis error minus rerum enim quos quas voluptatem voluptatum soluta hic delectus labore odio
+                    incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Sequi optio, perspiciatis error minus rerum enim quos quas voluptatem voluptatum soluta hic
+                    delectus labore odio incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit. Sequi optio, perspiciatis error minus rerum enim quos quas voluptatem
+                    voluptatum soluta hic delectus labore odio incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum
+                    dolor sit amet consectetur adipisicing elit. Sequi optio, perspiciatis error minus rerum enim quos
+                    quas voluptatem voluptatum soluta hic delectus labore odio incidunt asperiores cum quasi veritatis
+                    ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi optio, perspiciatis error minus
+                    rerum enim quos quas voluptatem voluptatum soluta hic delectus labore odio incidunt asperiores cum
+                    quasi veritatis ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi optio,
+                    perspiciatis error minus rerum enim quos quas voluptatem voluptatum soluta hic delectus labore odio
+                    incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Sequi optio, perspiciatis error minus rerum enim quos quas voluptatem voluptatum soluta hic
+                    delectus labore odio incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit. Sequi optio, perspiciatis error minus rerum enim quos quas voluptatem
+                    voluptatum soluta hic delectus labore odio incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum
+                    dolor sit amet consectetur adipisicing elit. Sequi optio, perspiciatis error minus rerum enim quos
+                    quas voluptatem voluptatum soluta hic delectus labore odio incidunt asperiores cum quasi veritatis
+                    ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi optio, perspiciatis error minus
+                    rerum enim quos quas voluptatem voluptatum soluta hic delectus labore odio incidunt asperiores cum
+                    quasi veritatis ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi optio,
+                    perspiciatis error minus rerum enim quos quas voluptatem voluptatum soluta hic delectus labore odio
+                    incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Sequi optio, perspiciatis error minus rerum enim quos quas voluptatem voluptatum soluta hic
+                    delectus labore odio incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit. Sequi optio, perspiciatis error minus rerum enim quos quas voluptatem
+                    voluptatum soluta hic delectus labore odio incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum
+                    dolor sit amet consectetur adipisicing elit. Sequi optio, perspiciatis error minus rerum enim quos
+                    quas voluptatem voluptatum soluta hic delectus labore odio incidunt asperiores cum quasi veritatis
+                    ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi optio, perspiciatis error minus
+                    rerum enim quos quas voluptatem voluptatum soluta hic delectus labore odio incidunt asperiores cum
+                    quasi veritatis ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi optio,
+                    perspiciatis error minus rerum enim quos quas voluptatem voluptatum soluta hic delectus labore odio
+                    incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Sequi optio, perspiciatis error minus rerum enim quos quas voluptatem voluptatum soluta hic
+                    delectus labore odio incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit. Sequi optio, perspiciatis error minus rerum enim quos quas voluptatem
+                    voluptatum soluta hic delectus labore odio incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum
+                    dolor sit amet consectetur adipisicing elit. Sequi optio, perspiciatis error minus rerum enim quos
+                    quas voluptatem voluptatum soluta hic delectus labore odio incidunt asperiores cum quasi veritatis
+                    ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi optio, perspiciatis error minus
+                    rerum enim quos quas voluptatem voluptatum soluta hic delectus labore odio incidunt asperiores cum
+                    quasi veritatis ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi optio,
+                    perspiciatis error minus rerum enim quos quas voluptatem voluptatum soluta hic delectus labore odio
+                    incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Sequi optio, perspiciatis error minus rerum enim quos quas voluptatem voluptatum soluta hic
+                    delectus labore odio incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit. Sequi optio, perspiciatis error minus rerum enim quos quas voluptatem
+                    voluptatum soluta hic delectus labore odio incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum
+                    dolor sit amet consectetur adipisicing elit. Sequi optio, perspiciatis error minus rerum enim quos
+                    quas voluptatem voluptatum soluta hic delectus labore odio incidunt asperiores cum quasi veritatis
+                    ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi optio, perspiciatis error minus
+                    rerum enim quos quas voluptatem voluptatum soluta hic delectus labore odio incidunt asperiores cum
+                    quasi veritatis ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi optio,
+                    perspiciatis error minus rerum enim quos quas voluptatem voluptatum soluta hic delectus labore odio
+                    incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Sequi optio, perspiciatis error minus rerum enim quos quas voluptatem voluptatum soluta hic
+                    delectus labore odio incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit. Sequi optio, perspiciatis error minus rerum enim quos quas voluptatem
+                    voluptatum soluta hic delectus labore odio incidunt asperiores cum quasi veritatis ab. 3 Lorem ipsum
+                    dolor sit amet consectetur adipisicing elit. Sequi optio, perspiciatis error minus rerum enim quos
+                    quas voluptatem voluptatum soluta hic delectus labore odio incidunt asperiores cum quasi veritatis
+                    ab.
+                </div>
+            </Tabs>
         </div>
     );
 }
---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import React, { MouseEvent as ReactMouseEvent, useEffect, useRef } from "react";
+
+import styles from "./Tabs.module.scss";
+
+interface TabsProps {
+    children: React.ReactNode[];
+}
+
+export default function Tabs({ children }: TabsProps) {
+    const headerListRef = useRef<HTMLUListElement>(null);
+    const contentRef = useRef<HTMLDivElement>(null);
+
+    /**
+     * Handles displaying the corresponding content when a header is clicked.
+     * It removes active and show classes from all elements, then adds the relevant ones
+     * to the clicked header and its corresponding content.
+     * @param e - The mouse event triggered by clicking a header.
+     * @param id - The identifier of the content to show.
+     */
+    function handleShowContent(e: ReactMouseEvent, id: string) {
+        // Get the header elements
+        const header = headerListRef.current!;
+        const headerElements = header.querySelectorAll("*");
+        headerElements.forEach((el) => el.classList.remove("active")); // Remove the 'active' class from all header elements
+
+        // Add the 'active' class to the clicked header element
+        const triggerEl = e.target as HTMLElement;
+        triggerEl.classList.add("active");
+
+        // Get the content elements
+        const content = contentRef.current!;
+        const contentElements = content.querySelectorAll("*");
+        contentElements.forEach((el) => {
+            el.classList.remove("show");
+        }); // Remove the 'show' class from all content elements
+
+        // Add the 'show' class to the target content element
+        const target = content.querySelector(`[data-target=${id}]`);
+        target?.classList.add("show");
+
+        return;
+    }
+
+    // Effect to validate children components on mount
+    useEffect(() => {
+        children.forEach((item) => {
+            if (React.isValidElement(item)) {
+                if (!item.props.id) {
+                    // Throw an error if the 'id' property is missing
+                    throw Error("Each child inside a 'Tabs' component must have a unique 'id' property.");
+                } else if (!item.props["data-header-text"]) {
+                    // Throw an error if the 'data-header-text' property is missing
+                    throw Error("Each child inside a 'Tabs' component must have a 'data-header-text' property.");
+                }
+            }
+        });
+    }, []);
+
+    return (
+        <div className={styles["tabs"]}>
+            <div className={styles["tabs__header"]}>
+                <ul className={styles["tabs__list"]} ref={headerListRef}>
+                    {children.map(
+                        (el, idx) =>
+                            React.isValidElement(el) && (
+                                <li key={idx} className={styles["tabs__item"]}>
+                                    <button
+                                        className={`${styles["tabs__btn"]} ${idx === 0 ? "active" : ""}`}
+                                        onClick={(e) => handleShowContent(e, el.props.id)}
+                                    >
+                                        {el.props["data-header-text"] || el.props.id}
+                                    </button>
+                                </li>
+                            )
+                    )}
+                </ul>
+            </div>
+            <div className={styles["tabs__content"]} ref={contentRef}>
+                {children.map(
+                    (item, idx) =>
+                        React.isValidElement(item) && (
+                            <div
+                                key={idx}
+                                className={`${styles["tabs__wrapper"]} ${idx === 0 ? "show" : ""}`}
+                                data-target={item.props.id}
+                            >
+                                {item}
+                            </div>
+                        )
+                )}
+            </div>
+        </div>
+    );
+}
